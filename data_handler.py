9bfc15ac1364ba4457ee0d6a9b4dab11fa4705e4
maciejlewicki95 <maciej.lewicki1995@gmail.com>
2021-02-18 19:55:38 +0100

    slowly but forward

--- data_handler.py
+++ data_handler.py
@@ -1,14 +1,40 @@
 import csv
 import os
 
-QUESTION_FILE_PATH = os.getenv('QUESTION_FILE_PATH') if 'QUESTION_FILE_PATH' in os.environ else 'sample_data/question.csv'
+QUESTION_FILE_PATH = os.getenv('QUESTION_FILE_PATH') if 'QUESTION_FILE_PATH' in os.environ else\
+    'C:/Users/macie/PycharmProjects/ask-mate-1-python-maciejlewicki95/sample_data/question.csv'
 QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
 
 def get_all_questions():
     questions = []
     with open(QUESTION_FILE_PATH) as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',', quotechar='|')
+        csv_reader = csv.DictReader(csv_file, delimiter=';', quotechar='|')
         for row in csv_reader:
             questions.append(row)
     return questions
+
+
+def get_next_id():
+    stories = get_all_questions()
+    current_max = 0
+    for story in stories:
+        if int(story['id']) >= current_max:
+            current_max = int(story['id'])
+
+    return current_max + 1
+
+
+def get_one_question(single_id):
+    questions = get_all_questions()
+    single_question = []
+    for question in questions:
+        if int(single_id) == int(question['id']):
+            single_question.append(question)
+    return single_question
+
+
+def save_user_story(question):
+    with open(QUESTION_FILE_PATH, 'a', newline="") as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=QUESTION_HEADER, delimiter=';', quotechar='|')
+        csv_writer.writerow(question)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 import data_handler
 
@@ -9,9 +9,30 @@ app = Flask(__name__)
 @app.route('/list')
 def route_list():
     questions = data_handler.get_all_questions()
-    headers = data_handler.get_headers()
+    headers = data_handler.QUESTION_HEADER
 
     return render_template('list.html', questions=questions, headers=headers)
+
+
+@app.route('/add-question')
+def display_add_question():
+    return render_template('add.html')
+
+
+@app.route('/add-question', methods=['POST'])
+def add_question():
+    question = dict(request.form)
+    question['id'] = data_handler.get_next_id()
+    data_handler.save_user_story(question)
+    print(question)
+    return redirect(url_for('route_list'))
+
+
+@app.route('/question/<int:user>')
+def display_one_question(user):
+    question = data_handler.get_one_question(user)
+    return render_template('one_question.html', question=question)
+
 
 
 if __name__ == "__main__":
