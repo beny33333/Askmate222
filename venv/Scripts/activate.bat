acc6766a69579f7b485ab42866302f084b217b1b
Beniamin Jarczyk <beniamin.jarczyk@gmail.com>
2021-03-04 23:50:46 +0100

    almost done

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\macie\PycharmProjects\super-sprinter-3000-python-maciejlewicki95\venv
+set VIRTUAL_ENV=C:\SS3000\flaskProject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
